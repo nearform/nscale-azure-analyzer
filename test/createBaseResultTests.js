e6dfef2ee379e77e5c953e30358dc81df18bf9b5
Dean McDonnell <mcdonnelldean@outlook.com>
2015-03-07 23:26:45 +0000

    Base Result Changes - updated createBaseResult.js to not require system and add refactoring - removed validateConfig.js and tests, it is no longer needed - updated sample-system.json to be a correct match to a real system - modified analyzer due to above

---
+++ test/createBaseResultTests.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var expect = require('must');
+var _ = require('lodash');
+
+var createBaseResult = require('../lib/createBaseResult.js');
+var sampleSystem = require('./data/sample-system.json');
+
+describe('createBaseResult.js:', function() {
+  var testSystem;
+
+  beforeEach(function() {
+    testSystem = _.cloneDeep(sampleSystem);
+  });
+
+  it('should err on missing system', function(done) {
+    createBaseResult(null, function(err, result) {
+      expect(err).to.be.truthy();
+
+      expect(result).to.be.falsy();
+
+      done();
+    });
+  });
+
+
+  it('should not err on valid system', function(done) {
+    createBaseResult(testSystem, function(err, result) {
+      expect(err).to.be.null();
+
+      expect(result).to.be.truthy();
+
+      done();
+    });
+  });
+
+  it('should have a null topology.name if none was found in system', function(done) {
+    delete testSystem.topology.name;
+
+    createBaseResult(testSystem, function(err, result) {
+      expect(err).to.be.null();
+
+      expect(result).to.be.truthy();
+      expect(result.topology.name).to.be.eql(null);
+
+      done();
+    });
+  });
+
+  it('should err if missing name in system', function(done) {
+    delete testSystem.name;
+
+    createBaseResult(testSystem, function(err, result) {
+      expect(err).to.be.truthy();
+      expect(result).to.be.falsy();
+      done();
+    });
+  });
+
+  it('should err if missing namespace in system', function(done) {
+    delete testSystem.namespace;
+
+    createBaseResult(testSystem, function(err, result) {
+      expect(err).to.be.truthy();
+      expect(result).to.be.falsy();
+      done();
+    });
+  });
+
+  it('should err if missing id in system', function(done) {
+    delete testSystem.id;
+
+    createBaseResult(testSystem, function(err, result) {
+      expect(err).to.be.truthy();
+      expect(result).to.be.falsy();
+      done();
+    });
+  });
+});
